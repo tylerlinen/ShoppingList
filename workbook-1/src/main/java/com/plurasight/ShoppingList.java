47c9d4e7e501c7bdd98651cbca592b297d852f90
Tyler <tylerlinen@gmail.com>
2023-10-05 15:25:01 -0400

    Completed ShoppingList app

---
+++ workbook-1/src/main/java/com/plurasight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.plurasight;
+
+public class ShoppingList {
+    public  static void main(String[] args) {
+        System.out.println("hats");
+        System.out.println("gloves");
+        System.out.println("shirts");
+        System.out.println("milk");
+        System.out.println("cookies");
+        System.out.println("shoes");
+        System.out.println("scarf");
+        System.out.println("bandages");
+        System.out.println("plates");
+        System.out.println("forks");
+    }
+}
